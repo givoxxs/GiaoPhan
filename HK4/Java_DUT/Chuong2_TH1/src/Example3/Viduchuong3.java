55399042e8623978b112152fc4af112b8a4da522
givoxxs <toanyogame@gmail.com>
2024-02-21 13:12:07 +0700

    Update file

---
+++ HK4/Java_DUT/Chuong2_TH1/src/Example3/Viduchuong3.java
@@ -0,0 +1,11 @@
+package Example3;
+
+public class Viduchuong3 {
+	public void hienthi() {
+		System.out.println("123");
+	}
+	public static void main(String[] args) {
+		Viduchuong3 dt = new Viduchuong3();
+		dt.hienthi();
+	}
+}
